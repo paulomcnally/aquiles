1735799e98439d9da1ef3aef932e5ffc3abec90a
Paulo McNally <paulomcnally@gmail.com>
2015-10-06 23:44:55 -0600

    finish login view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  # set layout
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
